b7bd5428d9c03faf60e3ff5bc53cce20903b6522
luisliquin <luisliquin@outlook.com.ar>
2024-12-31 18:57:38 -0300

    correccion de github

--- 02EntregaFinal/src/routes/user.Router.js
+++ 02EntregaFinal/src/routes/user.Router.js
@@ -12,10 +12,28 @@ const router = Router();
 router.post("/register", registerUser); // Ruta para registrar usuarios
 router.post("/login", loginUser);       // Ruta para login
 router.get("/logout", logoutUser);     // Ruta para logout
+
 router.get(
     "/current",
     passport.authenticate('jwtCookies', { session: false }),
     getCurrentUser
 ); // Ruta para obtener el usuario actual
+
+router.get('/github',
+     passport.authenticate('github', {scope: ['user:email']}), 
+     (req, res) => {
+    res.send({
+        status: 'success',
+        message: 'Success'
+    });
+});
+
+router.get('/github/callback',
+    passport.authenticate('github', { failureRedirect: '/login' }),
+    (req, res) => {
+        req.session.user = req.user;
+        res.redirect('/home');
+    }
+);
 
 export default router;

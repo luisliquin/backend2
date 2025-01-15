50d4bc373737dea6c073e6091bb966358e29f448
luisliquin <luisliquin@outlook.com.ar>
2025-01-14 21:07:34 -0300

    correccion carrito - producto

--- 02EntregaFinal/src/dto/cart.dto.js
+++ 02EntregaFinal/src/dto/cart.dto.js
@@ -1,11 +1,13 @@
 class CartDTO {
     constructor(cart) {
         this.id = cart._id;
-        this.products = cart.products.map(p => ({
-            productId: p.product._id,
-            name: p.product.name,
-            price: p.product.price,
-            quantity: p.quantity,
+        this.products = cart.products
+            .filter(p => p.product)
+            .map(p => ({
+                productId: p.product._id,
+                name: p.product.name,
+                price: p.product.price,
+                quantity: p.quantity,
         }));
     }
 }
